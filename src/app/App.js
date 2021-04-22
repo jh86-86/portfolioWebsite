8649bcdcac9bf3080c606eb716bda7b824494672
JJ <jh_86@hotmail.co.uk>
2021-04-22 12:37:28 +0100

    front and back connected

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,20 @@
-//import React,{useState,useEffect,useReducer} from 'react';
+import React,{useState,useEffect,useReducer} from 'react';
 import './App.css';
-
-import ProjectLinkPicture from '../Components/ProjectLinePicture/index';
-import {projectOne,projectTwo,projectThree} from '../ProjectObjects/projectObjects';
-import PersonalInfoCard from '../Components/PersonalInfo/index';
-import TechCard from '../Components/TechCard/index';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Bio from '../Pages/Bio';
-import Tech from '../Pages/Tech';
-import ProjectPage from '../Pages/ProjectPage';
 import Navbar from '../Router/Router'
 
 
 
 function App() {
-
+  useEffect(()=> {
+    async function getProjects(){
+      let url= "http://localhost:5000/projects";
+    let response =  await fetch(url);
+    let data = await response.json();
+      console.log(data);
+  
+    }
+    getProjects();
+  } ,[]);
 
   return (
     <div id="app">
