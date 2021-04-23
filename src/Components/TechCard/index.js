a33a5a0acb458bcff3db73e2d9aeafa807490fb9
JJ <jh_86@hotmail.co.uk>
2021-04-23 09:32:44 +0100

    backend stuff

--- src/Components/TechCard/index.js
+++ src/Components/TechCard/index.js
@@ -1,15 +1,26 @@
 
-import js from './js.png';
-import tailwind from './tailwind.png';
-import html from './html.png';
+import js from './frontend/js.png';
+import tailwind from './frontend/tailwind.png';
+import html from './frontend/html.png';
+import chakra from './frontend/chakra.png';
+import jest from './frontend/jest.jpeg';
+import node from './backend/node.png';
+import cSharp from './backend/c#.png';
+import nSubstitute from './backend/node.png';
+import Xunit from './backend/xunit.png';
+import dotnet from './backend/Net.png'
 
-let frontArray=[js,tailwind,html];
+
+let frontArray=[js,tailwind,html,chakra,jest];
+let backendArray=[node,cSharp,nSubstitute,Xunit,dotnet]
 
 function TechCard(){
     return(
         <div className="tech-card-layout">
-            <p>Tech stack </p>
-             {frontArray.map((pic)=>(<div><img className="tech-pics" alt="tech pics" src={pic} /></div>))}   
+            <p>Frontend Tech</p>
+             {frontArray.map((pic)=>(<div><img className="tech-pics" alt="tech pics" src={pic} /></div>))} 
+             <p>Backend Tech</p>  
+             {backendArray.map((pic)=>(<div><img className="tech-pics" alt="tech pics" src={pic} /></div>))}   
 
             
         </div>    
